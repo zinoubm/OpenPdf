afc7865ee928adbb858931b20c9afd00a0906386
zinoubm <zinne.eddine.2000@gmail.com>
2023-10-10 09:01:00 +0100

    Updating document picker and adding loading to delete operations

--- frontend/src/layout/MainLayout/Drawer/DrawerContent/index.js
+++ frontend/src/layout/MainLayout/Drawer/DrawerContent/index.js
@@ -1,51 +1,48 @@
 import { useState, useEffect } from 'react';
 
-import { Menu } from 'antd';
-import { FilePdfOutlined, UnorderedListOutlined } from '@ant-design/icons';
-import { FormControl, InputLabel, Select, Typography } from '@mui/material';
+import { Menu, Button } from 'antd';
+import { FilePdfOutlined, UnorderedListOutlined, DeleteOutlined } from '@ant-design/icons';
 
 import SimpleBar from 'components/third-party/SimpleBar';
-import DocumentPicker from './DocumentPicker/index';
 import useApi from 'api/hooks/useApi';
 
 import { useSelector, useDispatch } from 'react-redux';
-import { activeDocumentId, activeDocumentName } from 'store/reducers/app';
-
-function getItem(label, key, icon, children, type) {
-  return {
-    key,
-    icon,
-    children,
-    label,
-    type
-  };
-}
+import { activeDocumentId, activeDocumentName, updateRefresKey } from 'store/reducers/app';
 
 const DrawerContent = () => {
   const [documents, setDocuments] = useState([]);
-  const [menuItems, setMenuItems] = useState([]);
-
-  const { getDocuments } = useApi();
-
+  const [isLoading, setIsLoading] = useState(false);
+  const { getDocuments, deleteDocument } = useApi();
   const dispatch = useDispatch();
-  const { documentName, refreshKey } = useSelector((state) => state.app);
+  const { refreshKey } = useSelector((state) => state.app);
+
+  const MenuItem = ({ key, documentId, title, children, ...props }) => {
+    const handleDeleteDocument = (e) => {
+      e.stopPropagation();
+      setIsLoading(true);
+      deleteDocument(documentId).then(() => {
+        setIsLoading(false);
+        dispatch(updateRefresKey());
+      });
+    };
+    return (
+      <Menu.Item key={key} title={title} style={{ display: 'flex', justifyContent: 'space-between' }} {...props}>
+        {children}
+        <Button
+          loading={isLoading}
+          onClick={handleDeleteDocument}
+          danger
+          style={{ margin: '1em' }}
+          icon={<DeleteOutlined style={{ fontSize: '1em' }} />}
+        />
+      </Menu.Item>
+    );
+  };
 
   const handleSelectDocument = (e) => {
     dispatch(activeDocumentId({ documentId: e.key }));
     dispatch(activeDocumentName({ documentName: documents.find((document) => document.id == e.key).title }));
   };
-
-  useEffect(() => {
-    const itemsDocumentsList = documents.map((document) => getItem(document.title, document.id));
-    const items = [
-      getItem('Documents', 'documents-sub', <FilePdfOutlined />, itemsDocumentsList),
-      getItem('Collections', 'collections-sub', <UnorderedListOutlined />, []),
-      {
-        type: 'divider'
-      }
-    ];
-    setMenuItems(items);
-  }, [documents]);
 
   useEffect(() => {
     const fetchDocuments = async () => {
@@ -64,25 +61,17 @@ const DrawerContent = () => {
         }
       }}
     >
-      <Typography sx={{ color: 'transparent' }}>My Documents</Typography>
-      <FormControl fullWidth>
-        <InputLabel id="document-picker">Document</InputLabel>
-        <Select labelId="document-picker" id="document-selctor" value={documentName} label="Document">
-          {documents.map((document, index) => (
-            <DocumentPicker key={index} documentId={document.id} documentName={document.title} />
+      <Menu mode="inline">
+        <Menu.SubMenu key={'documents'} title={'Documents'} icon={<FilePdfOutlined />}>
+          {documents.map((document) => (
+            <MenuItem key={document.id} documentId={document.id} title={document.title} onClick={handleSelectDocument}>
+              {document.title}
+            </MenuItem>
           ))}
-        </Select>
-      </FormControl>
+        </Menu.SubMenu>
 
-      <Menu
-        onClick={handleSelectDocument}
-        style={{
-          width: 256
-        }}
-        defaultOpenKeys={['documents-sub']}
-        mode="inline"
-        items={menuItems}
-      />
+        <Menu.SubMenu key={'collections'} title={'Collections'} icon={<UnorderedListOutlined />}></Menu.SubMenu>
+      </Menu>
     </SimpleBar>
   );
 };
