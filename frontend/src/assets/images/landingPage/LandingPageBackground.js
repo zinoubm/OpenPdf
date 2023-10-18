a82b37608a4a7b63a7cfce3262cacbf352fe9815
zinoubm <zinne.eddine.2000@gmail.com>
2023-10-18 16:51:58 +0100

    fixing bakcground image size issues

--- frontend/src/assets/images/landingPage/LandingPageBackground.js
+++ frontend/src/assets/images/landingPage/LandingPageBackground.js
@@ -1,12 +1,28 @@
 import { Box } from '@mui/material';
-import background from './heroBackground.png';
+import background from 'assets/images/auth/openpdf-background.svg';
+import floating from './floating.png';
 
 const LandingPageBackground = () => {
   return (
-    <Box sx={{ position: 'absolute', overflow: 'hidden', zIndex: -1, bottom: 0 }}>
-      <div>
-        <img src={background} alt="openpdf-background" style={{ width: '150%', height: 'auto', transform: 'translate(-7em, 10em)' }} />
-      </div>
+    <Box
+      sx={{
+        width: '100%',
+        height: '110vh',
+        position: 'absolute',
+        overflow: 'hidden',
+        zIndex: -1
+      }}
+    >
+      <img
+        src={background}
+        alt="openpdf-background"
+        style={{ width: '150%', filter: 'blur(100px)', height: 'auto', position: 'absolute', top: '-32em', left: '6em', zIndex: 10 }}
+      />
+      <img
+        src={floating}
+        style={{ width: '45%', height: 'auto', position: 'relative', top: '10%', left: '52%', zIndex: 11 }}
+        alt="floating"
+      />
     </Box>
   );
 };
