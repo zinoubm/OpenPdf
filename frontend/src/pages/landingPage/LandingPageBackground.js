a033cbb47fc08c835c4ff06af9d0aa27921ab652
zinoubm <zinne.eddine.2000@gmail.com>
2023-10-26 16:47:18 +0100

    optimizing landing page and making it responsive

--- frontend/src/pages/landingPage/LandingPageBackground.js
+++ frontend/src/pages/landingPage/LandingPageBackground.js
@@ -1,6 +1,7 @@
 import { Box } from '@mui/material';
 import background from 'assets/images/auth/openpdf-background.svg';
-import floating from './floating.png';
+import floating from 'assets/images/floating.png';
+import './landingPage.css';
 
 const LandingPageBackground = () => {
   return (
@@ -15,10 +16,12 @@ const LandingPageBackground = () => {
     >
       <img
         src={background}
+        className="hero-background"
         alt="openpdf-background"
-        style={{ width: '150%', filter: 'blur(100px)', height: 'auto', position: 'absolute', top: '-32em', left: '6em', zIndex: 10 }}
+        style={{ width: '150%', filter: 'blur(80px)', height: 'auto', position: 'absolute', top: '-28em', left: '6em', zIndex: 10 }}
       />
       <img
+        className="hero-floating"
         src={floating}
         style={{ width: '45%', height: 'auto', position: 'relative', top: '10%', left: '52%', zIndex: 11 }}
         alt="floating"
--- frontend/src/pages/landingPage/index.js
+++ frontend/src/pages/landingPage/index.js
@@ -1,41 +1,45 @@
 import React from 'react';
 import { Box } from '@mui/material';
 import { Layout, Button, Typography } from 'antd';
-import LandingPageBackground from 'assets/images/landingPage/LandingPageBackground';
+import LandingPageBackground from 'pages/landingPage/LandingPageBackground';
 import Logo from 'assets/images/openpdfLogo.svg';
 
 const { Header } = Layout;
 
-import './style.css';
+import './landingPage.css';
 
 function LandingPage() {
   return (
     <Box>
       <LandingPageBackground />
       <Layout className="hero-layout" style={{ backgroundColor: 'transparent' }}>
-        <Header
-          className="hero-header"
-          style={{ height: '12vh', backgroundColor: 'transparent', display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}
-        >
-          <img className="hero-logo" src={Logo} style={{ width: '10em' }} alt="OpenPdf" />
-          <div className="hero-buttons-container" style={{ width: '20%' }}>
-            <Button className="hero-about-button" style={{ marginRight: '1em' }} type="text" href="#">
+        <Header className="hero-header">
+          <img className="hero-logo" src={Logo} alt="OpenPdf" />
+          <div className="hero-buttons-container">
+            <Button className="hero-about-button" type="text" href="#">
               About
             </Button>
-            <Button className="hero-cta-button" href="/login" style={{ backgroundColor: 'transparent', border: 'solid 1px black' }}>
+            <Button className="hero-cta-button" href="/login">
               Get Started
             </Button>
           </div>
         </Header>
-        <div style={{ display: 'flex' }}>
-          <div style={{ width: '50%', height: '100vh', padding: '4em' }}>
+        <div
+          className="hero-container"
+          // style={{ background: 'red' }}
+        >
+          <div
+            className="typography-container"
+            //  style={{ width: '50%', height: '100vh', padding: '4em' }}
+          >
             <Typography style={{ fontFamily: 'DM Serif Display, serif', fontSize: '4em', width: '75%' }}>
               Chat With Your Documents! No Hustle.
             </Typography>
             <Typography>Use AI and ask questions from your PDFs.</Typography>
             <Button
               href="/login"
-              style={{ backgroundColor: 'transparent', border: 'solid 1px black', marginTop: '1em', marginRight: '.6em' }}
+              className="hero-cta-button"
+              // style={{ backgroundColor: 'transparent', border: 'solid 1px black', marginTop: '1em', marginRight: '.6em' }}
             >
               Get Started For Free
             </Button>
@@ -43,7 +47,7 @@ function LandingPage() {
               Join Our Group Chat
             </Button>
           </div>
-          <div style={{ width: '50%', height: '100vh' }}>-</div>
+          {/* <div style={{ width: '50%', height: '100vh' }}>-</div> */}
         </div>
       </Layout>
     </Box>
